4de6fbc0f8e9afcf4e544266f43de351bc456f83
h.ozdemir <h.ozdemir@mersys.io>
2023-04-11 16:57:23 +0200

    first commit2

---
+++ src/Training/day37/main.java
@@ -0,0 +1,17 @@
+package Training.day37;
+
+public class main {
+    public static void main(String[] args) {
+        Araba auto1 = new Araba();
+        System.out.println(auto1.hiz);
+        System.out.println(auto1.rengi);
+
+        Araba auto2 = new Araba(10);
+        System.out.println(auto2.hiz);
+        System.out.println(auto2.rengi);
+
+
+    }
+
+
+}
